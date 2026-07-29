7e56a09fee4fde37ea0b4cadc3e43fba03562ee6
Prakash <kupraki@amazon.com>
2026-07-29 11:25:29 +0530

    chore(release): bump version to 0.4.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "app.clearsms"
         minSdk = 23
         targetSdk = 35
-        versionCode = 7
-        versionName = "0.4.0"
+        versionCode = 8
+        versionName = "0.4.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
